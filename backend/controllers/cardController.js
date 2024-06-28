524e1768cef5c1a53efd8e697bf4c8b23c533f6f
Kryptgen89 <146573536+Kryptgen89@users.noreply.github.com>
2024-06-27 23:20:01 -0400

    Initial commit for cardController.js

---
+++ backend/controllers/cardController.js
@@ -0,0 +1,17 @@
+// This will contain controllers for card management.
+
+export const getCards = (req, res) => {
+  // Controller logic to get all cards for the user.
+};
+
+export const addCard = (req, res) => {
+  // Controller logic to add a new card.
+};
+
+export const updateCard = (req, res) => {
+  // Controller logic to update an existing card.
+};
+
+export const deleteCard = (req, res) => {
+  // Controller logic to delete a card.
+};

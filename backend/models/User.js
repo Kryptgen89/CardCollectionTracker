b6595490fe9cc3fd87251812f03749571c41db66
Kryptgen89 <146573536+Kryptgen89@users.noreply.github.com>
2024-06-27 23:21:15 -0400

    Initial commit for User.js

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+// This will contain the User model schema.
+
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = mongoose.model('User', userSchema);
